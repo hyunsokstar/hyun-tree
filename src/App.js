6a7f922325354ba1394185cedae9053763895d35
hyun <hyunsokstar@gmail.com>
2022-11-19 20:42:20 +0900

    update for 검색 인풋 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import './App.css';
-import Tree from './components/Tree';
-
+import React from "react";
+import "./App.css";
+import Tree from "./components/Tree";
 
 function App() {
-  return (
-    <div className="App" 
-         style={
-                {
-                  width:"40%",
-                  border: "1px solid #c7c2c2",
-                  marginTop: "20px"
-                }
-              }
-    >
-      <Tree/>
-    </div>
-  );
+    return (
+        <div
+            className="App"
+            style={{
+                width: "40%",
+                border: "1px solid #c7c2c2",
+                marginTop: "20px",
+            }}
+        >
+            <Tree />
+        </div>
+    );
 }
 
 export default App;
